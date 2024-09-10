01a09a987cf29eb54e793115cde26c327c84ff89
Mac1618 <ma46099@gmail.com>
2024-09-10 11:26:38 +0800

    01-intro: Basic introduction to typescript and global installation

---
+++ 01-intro/intro.ts
@@ -0,0 +1,6 @@
+const user = { name: 'Mark Laurence', age: 24 };
+
+console.log('Mark Laurence');
+
+// we know it dont exist in user object but it will still run in normal js and wont show red line under the code.
+console.log(user.email);
