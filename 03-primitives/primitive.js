d634b92f24f0dd08aec5935786b57da27911976e
Mac1618 <ma46099@gmail.com>
2024-09-14 15:40:24 +0800

    02 Primitive types and 03 Any: learned about best practices and when to use any

---
+++ 03-primitives/primitive.js
@@ -0,0 +1,12 @@
+// The Primitives: string, number and boolean
+// NOTE: there is no int and float simply 'number'
+// - typescript automatically provide variable type if has a value
+// NUMBER: GOOD PRACTICE
+var xNumId = 135.23;
+// xNumId = 'mark laurence'; // error shows that this is number
+// NUMBER: BAD PRACTICE
+var yNumId = 166.23;
+// NUMBER: GOOD PRACTICE
+var isLoggedInX = false;
+// NUMBER: BAD PRACTICE
+var isLoggedInY = true;
---
+++ 03-primitives/primitive.ts
@@ -0,0 +1,19 @@
+// The Primitives: string, number and boolean
+// NOTE: there is no int and float simply 'number'
+// - typescript automatically provide variable type if has a value
+
+// NUMBER: GOOD PRACTICE
+let xNumId = 135.23;
+// xNumId = 'mark laurence'; // error shows that this is number
+
+// NUMBER: BAD PRACTICE
+let yNumId: number = 166.23;
+
+// BOOLEAN: GOOD PRACTICE
+let isLoggedInX = false;
+
+// BOOLEAN: BAD PRACTICE
+let isLoggedInY: boolean = true;
+
+// temporary mesure to remove error when using let
+export {};
