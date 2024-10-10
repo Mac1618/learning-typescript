da3184be2a065de288403f486de7f2aa531a38ae
Mac1618 <ma46099@gmail.com>
2024-10-10 14:17:28 +0800

    Lessson 9 Tuples: Giving a specific order of variable types inside an array

--- 09-tuples/myTuples.ts
+++ 09-tuples/myTuples.ts
@@ -2,4 +2,13 @@
 // Example 1:
 let tUser: [string, number, boolean] = ['Hello', 16, true];
 
+// Problem with Tuples
+// Example 2:
+type User = [string, number];
+const newUser: User = ['Mark', 18];
+
+// problems
+newUser[1] = 44; // the new user array value is changable
+newUser.push(22); // can push a string or number but not a boolean inside a defined array
+
 export {};
