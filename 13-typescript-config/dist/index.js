f71102230648bee8efe7540f61172c12682e16cc
Mac1618 <ma46099@gmail.com>
2024-10-13 19:17:42 +0800

    Lesson 15 Class: best practices of class with private and public

--- 13-typescript-config/dist/index.js
+++ 13-typescript-config/dist/index.js
@@ -1,15 +1,1 @@
 "use strict";
-console.log('Typescript is here');
-// -----------------------------------------------------------------------------------------------
-// LESSON 14: CLASS - basics
-class User {
-    constructor(email, name) {
-        this.city = 'San Pablo City'; // needs a value because it is not being defind in constructor
-        this.email = email;
-        this.name = name;
-    }
-}
-const newUser1 = new User('one@mc.com', 'Mac One');
-const newUser2 = new User('two@mc.com', 'Mac Two');
-console.log('User 1: ', newUser1);
-console.log('User 2: ', newUser2);
---
+++ 13-typescript-config/src/14-15_class.ts
@@ -0,0 +1,48 @@
+// -----------------------------------------------------------------------------------------------
+// LESSON 14: CLASS - basics
+
+class User14 {
+	// NOTE: variables here should have an initiator or a value
+	email: string;
+	name: string;
+	readonly city: string = 'San Pablo City'; // needs a value because it is not being defind in constructor
+
+	constructor(email: string, name: string) {
+		this.email = email;
+		this.name = name;
+	}
+}
+
+const newUser14_1 = new User14('one@14.com', 'Mac One - 14');
+const newUser14_2 = new User14('two@14.com', 'Mac Two - 14');
+console.log('User 14: ', newUser14_1);
+console.log('User 14: ', newUser14_2);
+
+// -----------------------------------------------------------------------------------------------
+// LESSON 15: CLASS - private, public moderator with practical usage.
+//  - there are two ways to use typescript in classs.
+//  - "public" is accessible outside the class.
+//  - "private" is only accessible inside the class.
+
+class User15 {
+	// 1ST METHOD:
+	// public email: string;
+	// public name: string;
+	// private readonly city: string = 'San Pablo City'; // private e.g. 1
+	// constructor(email: string, name: string) {
+	// 	this.email = email;
+	// 	this.name = email;
+	// }
+
+	// 2ND METHOD: PRACTICAL WAY
+	// - two ways to use private
+	private readonly city: string = 'San Pablo City'; // private e.g. 1
+	// readonly #city: string = 'San Pablo City' // private e.g. 2
+
+	constructor(public email: string, public name: string) {}
+}
+
+const newUser15_1 = new User15('one@15.com', 'Mac One - 15');
+const newUser15_2 = new User15('two@15.com', 'Mac Two - 15');
+console.log('User 15: ', newUser15_1);
+console.log('User 15: ', newUser15_2);
